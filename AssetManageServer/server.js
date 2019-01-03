f7eb9afbe5699185b1948cf0cb97e0cec0ce04da
liangxf <liangxf@skong.com>
2019-01-03 15:54:22 +0800

    【测试】【修改】【提交】

--- AssetManageServer/server.js
+++ AssetManageServer/server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser")
 var app = express();
 var sqlAction = require('./route/sqlRoute'); //在应用中加载路由模块：数据库操作相关操作路由
 
-app.use('/static', express.static('public')); //利用 Express 托管静态文件 
+app.use('/static', express.static('public')); //利用 Express 托管静态文件 测试
 app.use(bodyParser.urlencoded({
    extended: false,                 //扩展模式
    limit: 2 * 1024 * 1024           //限制-2M
